67092ed85a9166c25d4479e34c5e481008dea9d5
Akshat Lambhate <akshatlambhate180302@gmail.com>
2022-08-18 23:00:21 +0530

    buy buttons and hover options

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 var acc = document.getElementsByClassName("accordion");
 var i ;
- 
+var activeaccor = 1;
+
+  
 for(i=0;i<acc.length;i++){
+          //  var active =  document.getElementById(activeaccor);
+          //      active.classList.remove("active");
+          //   var activePannel = active.nextElementSibling;
+          //   activePannel.style.display === "none";
         acc[i].addEventListener("click", function() {
+                
+                // activeaccor = this.id;
                 this.classList.toggle("active");
         
         var pannel = this.nextElementSibling;
+       
         if (pannel.style.display === "block") {
           pannel.style.display = "none";
         } else {
           pannel.style.display = "block";
         }
+        
+        
 });
 }
+
 function changeimg(){
   const image = [
     "media/BTC icon.svg",
@@ -25,4 +37,10 @@ function changeimg(){
   
 }
 setInterval(changeimg ,1500);
+
+let hovermenu = document.getElementsByClassName("media")
+let displaymenu = document.getElementsByClassName("hovermenu")
+ hovermenu.addEventListener("hover", function(){
+    displaymenu.style.visibility = true;
+ })
 
